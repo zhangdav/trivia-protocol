2b778802b20b8c6d08c3e27f83670b6194fdc5d4
Julio M Cruz <julio.cruz@eb-ms.net>
2024-05-22 15:02:08 -0400

    Update Header.tsx

--- trivia-dex/components/Header.tsx
+++ trivia-dex/components/Header.tsx
@@ -13,17 +13,32 @@ export default function HeaderComponent() {
 
   return (
     <header className="absolute top-0 left-0 right-0 flex justify-between items-center p-4">
-        <h1 className="text-5xl ml-8">Trivia</h1>
-        { !isConnected && (
-          <Button className="text-[#70f7c9] border-[#70f7c9] mr-8" variant="outline" onClick={openConnectModal}>
-            Connect Wallet
-          </Button>
-        )}
+          <Link href="/">
+            <h1 className="text-5xl ml-8">Trivia</h1>
+          </Link> 
           { isConnected && (
-          <div className="mx-8">
-            <ConnectButton />
-          </div>
-        )}
+          <nav className="hidden md:flex mt-6">
+            <Link className="text-gray-300 hover:text-white text-2xl mx-4" href="#">
+              Quiz
+            </Link>
+            <Link className="text-gray-300 hover:text-white text-2xl mx-4" href="/dashboard">
+              Prize
+            </Link>
+            <Link className="text-gray-300 hover:text-white text-2xl mx-4" href="#">
+              Account
+            </Link>
+          </nav>          
+          )}
+          { !isConnected && (
+            <Button className="text-[#70f7c9] border-[#70f7c9] mr-8  mt-6" variant="outline" onClick={openConnectModal}>
+              Connect Wallet
+            </Button>
+          )}
+            { isConnected && (
+            <div className="mr-8  mt-6">
+              <ConnectButton />
+            </div>
+          )}
     </header>
     );
 }
